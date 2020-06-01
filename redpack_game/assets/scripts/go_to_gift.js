8b5f49d60a14f74ef196c12a0cd263613cc28142
张峻郡 <zhangjunjun@xinhua.org>
2020-06-01 17:25:08 +0800

    修改了appid

--- redpack_game/assets/scripts/go_to_gift.js
+++ redpack_game/assets/scripts/go_to_gift.js
@@ -1,9 +1,7 @@
 cc.Class({
   extends: cc.Component,
 
-  properties: {
-
-  },
+  properties: {},
 
   // LIFE-CYCLE CALLBACKS:
 
@@ -16,10 +14,12 @@ cc.Class({
 
   to_gift(e) {
     e.stopPropagation();
-    const oauth2_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6d4c967a36137d82" +
-      "&redirect_uri=" + encodeURI("http://tongan-gov.jihoukeji.vip/gift.html") +
+    const oauth2_url =
+      "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx836c7d977708e1a7" +
+      "&redirect_uri=" +
+      encodeURI("http://tongan-gov.jihoukeji.vip/gift.html") +
       "&response_type=code&scope=snsapi_userinfo&state=oauth_approved#wechat_redirect";
     window.location.href = oauth2_url;
     // cc.director.loadScene('gift');
-  }
+  },
 });
